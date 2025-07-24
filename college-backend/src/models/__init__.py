34bb96c3d376753e55dd53ad5b10dbe69175bb6a
Vimal Kumar <vkvimal14@gmail.com>
2025-07-24 20:16:25 +0530

    clkh

--- college-backend/src/models/__init__.py
+++ college-backend/src/models/__init__.py
@@ -1,5 +1,5 @@
 from .user import User
-from .college import College
+from .college import Admin
 from .notification import Notification
 from .gallery import GalleryItem
 from .facility import Facility
